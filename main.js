98f817beedf47fe744a1e4b183dbcdd4e114eaf9
BBMinhh <toitenbinhminh@gmail.com>
2023-07-06 19:54:24 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var myDog = {
+    "name": "Ngáo",
+    "legs": 4,
+    "friends": ["everything!"]
+};
+myDog.name = "Husky"
+console.log(myDog.name);
+
+myDog.color = "Brown";
+delete myDog.friends;
---
+++ mainn.js
@@ -0,0 +1,4 @@
+const gio = [["Grape",15],["Apple",3],["Banana",9]]
+
+console.log("I have" + " " +  gio[0][1] + " "+ gio[0][0]);
+
